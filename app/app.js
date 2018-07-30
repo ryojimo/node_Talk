0f56705a37553a04213f48acce3786327b51a65f
ryojimo <ryojimo@gmail.com>
2018-07-30 22:34:16 +0900

    first commit

---
+++ app/app.js
@@ -0,0 +1,219 @@
+/**
+ * @fileoverview アプリケーション UI
+ * @author       Ryoji Morita
+ * @version      0.0.1
+*/
+//var sv_ip   = 'sensor.rp.lfx.sony.co.jp';   // node.js server の IP アドレス
+//var sv_ip   = '43.31.78.45';                // node.js server の IP アドレス
+var sv_ip   = '192.168.91.11';                // node.js server の IP アドレス
+var sv_port = 2000;                           // node.js server の port 番号
+
+var server = io.connect( 'http://' + sv_ip + ':' + sv_port ); //ローカル
+
+
+//-----------------------------------------------------------------------------
+//-------------------------------------
+// ブラウザオブジェクトから受け取るイベント
+window.onload = function(){
+  console.log( "[app.js] window.onloaded" );
+};
+
+
+window.onunload = function(){
+  console.log( "[app.js] window.onunloaded" );
+};
+
+
+//-----------------------------------------------------------------------------
+// サーバから受け取るイベント
+server.on( 'connect', function(){               // 接続時
+  console.log( "[app.js] " + 'connected' );
+});
+
+
+server.on( 'disconnect', function( client ){    // 切断時
+  console.log( "[app.js] " + 'disconnected' );
+});
+
+
+server.on( 'S_to_C_DATA', function( data ){
+  console.log( "[app.js] " + 'S_to_C_DATA' );
+  console.log( "[app.js] data = " + data.value );
+//  window.alert( 'コマンドを送信しました。\n\r' + data.value );
+
+  document.getElementById('val_sensor').innerHTML = data.value; // 数値を表示
+});
+
+
+//-----------------------------------------------------------------------------
+// ドキュメント・オブジェクトから受け取るイベント
+
+
+//-----------------------------------------------------------------------------
+/**
+ * Get コマンドを送る。
+ * @param {string} cmd - コマンドの文字列
+ * @return {void}
+ * @example
+ * sendGetCmd( 'sudo ./board.out temp1' );
+*/
+function sendGetCmd( cmd ){
+  console.log( "[app.js] sendGetCmd()" );
+  console.log( "[app.js] cmd = " + cmd );
+
+  console.log( "[app.js] server.emit(" + 'C_to_S_GET' + ")" );
+  server.emit( 'C_to_S_GET', cmd );
+}
+
+
+/**
+ * Set コマンドを送る。
+ * @param {string} cmd - コマンドの文字列
+ * @return {void}
+ * @example
+ * sendSetCmd( 'sudo ./board.out relay on' );
+*/
+function sendSetCmd( cmd ){
+  console.log( "[app.js] sendSetCmd()" );
+  console.log( "[app.js] cmd = " + cmd );
+
+  console.log( "[app.js] server.emit(" + 'C_to_S_SET' + ")" );
+  server.emit( 'C_to_S_SET', cmd );
+}
+
+
+/**
+ * コメントのデータを送信する
+ * @param {void}
+ * @return {void}
+ * @example
+ * sendCmnt();
+*/
+function sendCmnt(){
+  console.log( "[app.js] sendCmnt()" );
+
+  // データをチェック
+  var cmnt = document.getElementById( 'val_cmnt' );
+//  console.log( "[app.js] cmnt.value =" + cmnt.value );
+
+  // サーバーへデータを送信
+  if( cmnt.value == '' ){
+    alert( 'ご要望・ご意見を記入してください。' );
+  } else{
+    console.log( "[app.js] server.emit(" + 'C_to_S_CMNT' + ")" );
+    server.emit( 'C_to_S_CMNT', cmnt.value );
+  }
+
+  // データをクリア
+  clearCmnt();
+}
+
+
+/**
+ * コメントをクリアする
+ * @param {void}
+ * @return {void}
+ * @example
+ * clearCmnt();
+*/
+function clearCmnt(){
+  console.log( "[app.js] clearCmnt()" );
+  var cmnt = document.getElementById( 'val_cmnt' );
+  cmnt.value = '';
+}
+
+
+/**
+ * Music コマンド ( PLAY, PAUSE, STOP, RESUME ) を送る。
+ * @param {string} cmd - 'start'/'stop'
+ * @return {void}
+ * @example
+ * sendMusicCmd( 'PLAY' );
+*/
+function sendMusicCmd( cmd ){
+  console.log( "[app.js] sendMusicCmd()" );
+  console.log( "[app.js] cmd = " + cmd );
+
+  console.log( "[app.js] server.emit(" + 'C_to_S_MUSIC' + ")" );
+  server.emit( 'C_to_S_MUSIC', cmd );
+}
+
+
+/**
+ * しゃべる文字データを送る。
+ * @param {string} cmnt - しゃべる文字列
+ * @return {void}
+ * @example
+ * sendTalkData( cmnt );
+*/
+function sendTalkData( cmnt ){
+  console.log( "[app.js] sendTalkData()" );
+  console.log( "[app.js] cmnt = " + cmnt );
+
+  console.log( "[app.js] server.emit(" + 'C_to_S_TALK' + ")" );
+  server.emit( 'C_to_S_TALK', cmnt );
+}
+
+
+/**
+ * Mic 入力 / 停止
+ * @param {void}
+ * @return {void}
+ * @example
+ * submitMicStart(); / submitMicStop();
+*/
+window.SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
+var recognition = new webkitSpeechRecognition();
+recognition.lang = 'ja';
+
+
+// 録音終了時トリガー
+recognition.addEventListener( 'result', function(event){
+    var text = event.results.item(0).item(0).transcript;
+    console.log( "[app.js] text = " + text );
+    $('#val_cmnt').val( text );
+}, false );
+
+
+function submitMicStart(){
+  console.log( "[app.js] submitMicStart()" );
+
+  var hi = 'ご用件をどうぞ';
+
+  sendTalkData( hi );
+//  recognition.start();
+}
+
+
+function submitMicStop(){
+  console.log( "[app.js] submitMicStop()" );
+  recognition.stop();
+}
+
+
+/**
+ * トークのデータを送信する
+ * @param {void}
+ * @return {void}
+ * @example
+ * sendTalk();
+*/
+function sendTalk(){
+  console.log( "[app.js] sendTalk()" );
+
+  var talker = $('#val_talker').val();
+  var cmnt   = $('#val_talk').val();
+  console.log( "[app.js] talker = " + talker );
+  console.log( "[app.js] cmnt   = " + cmnt );
+
+  // サーバーへデータを送信
+  if( cmnt == '' ){
+    alert( '話す内容を記入してください。' );
+  } else{
+    var obj = { talker:talker, cmnt:cmnt };
+    console.log( "[app.js] server.emit(" + 'C_to_S_TALK_W_NAME' + ")" );
+    server.emit( 'C_to_S_TALK_W_NAME', obj );
+  }
+}
+
+
---
+++ main.js
@@ -0,0 +1,324 @@
+/**
+ * @fileoverview メイン・システム
+ * @author       Ryoji Morita
+ * @version      0.0.1
+*/
+
+// 必要なライブラリをロード
+var http     = require( 'http' );
+var socketio = require( 'socket.io' );
+var fs       = require( 'fs' );
+var colors   = require( 'colors' );
+require( 'date-utils' );
+var schedule = require( 'node-schedule' );
+
+const DataCmnt   = require( './js/DataCmnt' );
+const DataPerson = require( './js/DataPerson' );
+const DataSensors = require( './js/DataSensors' );
+const Docomo     = require( './js/Docomo' );
+const PlayMusic  = require( './js/PlayMusic' );
+
+
+// Ver. 表示
+var now = new Date();
+console.log( "[main.js] " + now.toFormat("YYYY年MM月DD日 HH24時MI分SS秒").rainbow );
+console.log( "[main.js] " + "ver.01 : app.js".rainbow );
+console.log( "[main.js] " + "access to http://localhost:3000" );
+
+// サーバー・オブジェクトを生成
+var server = http.createServer();
+
+// request イベント処理関数をセット
+server.on( 'request', doRequest );
+
+// 待ち受けスタート
+server.listen( process.env.VMC_APP_PORT || 3000 );
+console.log( "[main.js] Server running!" );
+
+// request イベント処理
+function doRequest(
+  req,    // http.IncomingMessage オブジェクト : クライアントからのリクエストに関する機能がまとめられている
+  res     // http.serverResponse  オブジェクト : サーバーからクライアントへ戻されるレスポンスに関する機能がまとめられている
+){
+  switch( req.url ){
+  case '/':
+    fs.readFile( './app/app.html', 'UTF-8',
+      function( err, data ){
+        if( err ){
+          res.writeHead( 404, {'Content-Type': 'text/html'} );
+          res.write( 'File Not Found.' );
+          res.end();
+          return;
+        }
+        res.writeHead( 200, {'Content-Type': 'text/html',
+                             'Access-Control-Allow-Origin': '*'
+                      } );
+        res.write( data );
+        res.end();
+      }
+    );
+  break;
+  case '/app.js':
+    fs.readFile( './app/app.js', 'UTF-8',
+      function( err, data ){
+        res.writeHead( 200, {'Content-Type': 'application/javascript',
+                             'Access-Control-Allow-Origin': '*'
+                      } );
+        res.write( data );
+        res.end();
+      }
+    );
+  break;
+  case '/style.css':
+    fs.readFile( './app/style.css', 'UTF-8',
+      function( err, data ){
+        res.writeHead( 200, {'Content-Type': 'text/css',
+                             'Access-Control-Allow-Origin': '*'
+                      } );
+        res.write( data );
+        res.end();
+      }
+    );
+  break;
+  case '/bg.gif':
+    fs.readFile( './app/bg.gif', 'binary',
+      function( err, data ){
+        res.writeHead( 200, {'Content-Type': 'image/gif',
+                             'Access-Control-Allow-Origin': '*'
+                      } );
+        res.write( data, 'binary' );
+        res.end();
+      }
+    );
+  break;
+  case '/jQueryRotate.js':
+    fs.readFile( './app/js_lib/jQueryRotate.js', 'UTF-8',
+      function( err, data ){
+        res.writeHead( 200, {'Content-Type': 'application/javascript',
+                             'Access-Control-Allow-Origin': '*'
+                      } );
+        res.write( data );
+        res.end();
+      }
+    );
+  break;
+  case '/tmp/picture.jpg':
+    fs.readFile( './tmp/picture.jpg', 'binary',
+      function( err, data ){
+        res.writeHead( 200, {'Content-Type': 'image/jpg',
+                             'Access-Control-Allow-Origin': '*'
+                      } );
+        res.write( data, 'binary' );
+        res.end();
+      }
+    );
+  break;
+  }
+}
+
+
+var io = socketio.listen( server );
+
+
+//-----------------------------------------------------------------------------
+// 起動の処理関数
+//-----------------------------------------------------------------------------
+var timerFlg;
+
+var cmnt    = new DataCmnt();
+
+var docomo  = new Docomo();
+var music   = new PlayMusic();
+var music_pid = 0;
+
+
+startSystem();
+
+
+/**
+ * システムを開始する
+ * @param {void}
+ * @return {void}
+ * @example
+ * startSystem();
+*/
+function startSystem() {
+  console.log( "[main.js] startSystem()" );
+};
+
+
+//-----------------------------------------------------------------------------
+// クライアントからコネクションが来た時の処理関数
+//-----------------------------------------------------------------------------
+io.sockets.on( 'connection', function( socket ){
+
+  // 切断したときに送信
+  socket.on( 'disconnect', function(){
+    console.log( "[main.js] " + 'disconnect' );
+//  io.sockets.emit('S_to_C_DATA', {value:'user disconnected'});
+  });
+
+
+  // Client to Server
+  socket.on( 'C_to_S_NEW', function( data ){
+    console.log( "[main.js] " + 'C_to_S_NEW' );
+  });
+
+
+  socket.on( 'C_to_S_DELETE', function( data ){
+    console.log( "[main.js] " + 'C_to_S_DELETE' );
+  });
+
+
+  socket.on( 'C_to_S_GET', function( data ){
+    console.log( "[main.js] " + 'C_to_S_GET' );
+    console.log( "[main.js] data = " + data );
+
+    var exec = require( 'child_process' ).exec;
+    var ret  = exec( data,
+      function( err, stdout, stderr ){
+        console.log( "[main.js] stdout = " + stdout );
+        console.log( "[main.js] stderr = " + stderr );
+        if( err ){
+          console.log( "[main.js] " + err );
+        }
+
+        io.sockets.emit( 'S_to_C_DATA', {value:stdout} );
+    });
+  });
+
+
+  socket.on( 'C_to_S_SET', function( data ){
+    console.log( "[main.js] " + 'C_to_S_SET' );
+    console.log( "[main.js] data = " + data );
+
+    var exec = require( 'child_process' ).exec;
+    var ret  = exec( data,
+      function( err, stdout, stderr ){
+        console.log( "[main.js] stdout = " + stdout );
+        console.log( "[main.js] stderr = " + stderr );
+        if( err ){
+          console.log( "[main.js] " + err );
+        }
+      });
+  });
+
+
+  socket.on( 'C_to_S_CMNT', function( data ){
+    console.log( "[main.js] " + 'C_to_S_CMNT' );
+    console.log( "[main.js] data = " + data );
+
+    var data = { time: hhmmss(), cmnt: data }
+    var file = '/media/pi/USBDATA/' + yyyymmdd() + '_cmnt.txt';
+    console.log( "[main.js] data.time = " + data.time );
+    console.log( "[main.js] data.cmnt = " + data.cmnt );
+    console.log( "[main.js] file = " + file );
+
+    cmnt.Update( data );
+    cmnt.AppendFile( file );
+    var ret = cmnt.ReadFile( file );
+  });
+
+
+  socket.on( 'C_to_S_MUSIC', function( data ){
+    console.log( "[main.js] " + 'C_to_S_MUSIC' );
+
+    if( data == 'GET PID' ){
+      music.GetPID( function( id ){
+        music_pid = id;
+        console.log( "[main.js] " + "pid=" + music_pid );
+      });
+    } else if( data == 'PLAY' ){
+      music.Play( 'Music.mp3' );
+    } else {
+      music.ChangeStatus( data );
+    }
+  });
+
+
+  socket.on( 'C_to_S_TALK', function( cmnt ){
+    console.log( "[main.js] " + 'C_to_S_TALK' );
+    console.log( "[main.js] cmnt = " + cmnt );
+
+    docomo.Update( 'nozomi', 'hello' );
+    docomo.Talk( cmnt, function(){
+      io.sockets.emit( 'S_to_C_TALK_CB', {value:true} )
+    });
+  });
+
+
+  socket.on( 'C_to_S_TALK_W_NAME', function( data ){
+    console.log( "[main.js] " + 'C_to_S_TALK_W_NAME' );
+    console.log( "[main.js] data = " + data );
+    console.log( "[main.js] data.talker = " + data.talker );
+    console.log( "[main.js] data.cmnt   = " + data.cmnt );
+
+    docomo.Update( data.talker , 'hello' );
+    docomo.Talk( data.cmnt, function(){
+    });
+  });
+
+
+});
+
+
+/**
+ * 数字が 1 桁の場合に 0 埋めで 2 桁にする
+ * @param {number} num - 数値
+ * @return {number} num - 0 埋めされた 2 桁の数値
+ * @example
+ * toDoubleDigits( 8 );
+*/
+var toDoubleDigits = function( num ){
+//  console.log( "[main.js] toDoubleDigits()" );
+//  console.log( "[main.js] num = " + num );
+  num += '';
+  if( num.length === 1 ){
+    num = '0' + num;
+  }
+  return num;
+};
+
+
+/**
+ * 現在の日付を YYYY-MM-DD 形式で取得する
+ * @param {void}
+ * @return {string} day - 日付
+ * @example
+ * yyyymmdd();
+*/
+var yyyymmdd = function(){
+  console.log( "[main.js] yyyymmdd()" );
+  var date = new Date();
+
+  var yyyy = date.getFullYear();
+  var mm   = toDoubleDigits( date.getMonth() + 1 );
+  var dd   = toDoubleDigits( date.getDate() );
+
+  var day = yyyy + '-' + mm + '-' + dd;
+  console.log( "[main.js] day = " + day );
+  return day;
+};
+
+
+/**
+ * 現在の時刻を HH:MM:SS 形式で取得する
+ * @param {void}
+ * @return {string} time - 時刻
+ * @example
+ * hhmmss();
+*/
+var hhmmss = function(){
+  console.log( "[main.js] hhmmss()" );
+  var date = new Date();
+
+  var hour = toDoubleDigits( date.getHours() );
+  var min  = toDoubleDigits( date.getMinutes() );
+  var sec  = toDoubleDigits( date.getSeconds() );
+
+  var time = hour + ':' + min + ':' + sec;
+  console.log( "[main.js] time = " + time );
+  return time;
+};
+
+
